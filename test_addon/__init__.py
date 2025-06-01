720a27cbaad2ce753dfeec8af9a2d2a940ed1a1f
MiguelVivar <miguelvivarfarfan@gmail.com>
2025-06-01 17:20:29 -0500

    Add changes

---
+++ test_addon/__init__.py
@@ -0,0 +1,2 @@
+# -*- coding: utf-8 -*-
+# Test addon - empty init file
---
+++ test_addon/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    'name': 'Test Addon',
+    'version': '1.0.0',
+    'category': 'Tools',
+    'summary': 'Simple test addon for upload testing',
+    'description': """
+        This is a simple test addon to verify upload functionality.
+    """,
+    'depends': ['base'],
+    'data': [],
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
